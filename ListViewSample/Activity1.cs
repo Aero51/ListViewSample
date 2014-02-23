5a0cfdedb7f78ea21724caa269c949d22bf2ca12
Aero51 <nikola.srdoc@grshpr.com>
2014-02-23 02:05:57 +0100

    set up mockup data in first(active) item

--- ListViewSample/Activity1.cs
+++ ListViewSample/Activity1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 using Android.App;
 using Android.Content;
@@ -13,15 +14,27 @@ namespace ListViewSample
     public class Activity1 : Activity
     {
 
-		ListView assignmentsListView;
-		LinearLayout assignmentActiveLayout;
-		TextView number,
-		name,
-		job,
-		phone,
-		address,
-		timerText;
+        ListView assignmentsListView, 
+            assignmentNameListView, 
+            assignmentJobListView, 
+            assignmentPhoneListView,
+            assignmentAddressListView;
 
+        LinearLayout assignmentActiveLayout;
+
+        TextView number,
+            name,
+            job,
+            phone,
+            address,
+            timerText;
+
+
+        ArrayList list_item_number = new ArrayList();
+        ArrayList list_assingment_name = new ArrayList();
+        ArrayList list_assingment_job = new ArrayList();
+        ArrayList list_assingment_phone = new ArrayList();
+        ArrayList list_assingment_Address = new ArrayList();
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -31,68 +44,63 @@ namespace ListViewSample
 
             base.OnCreate(bundle);
 			//SetContentView(Resource.Layout.Main); 
-
-
-
 			//var listView = FindViewById<ListView>(Resource.Id.listView1);
 			//var data = new string[] { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
 			//listView.Adapter = new ArrayAdapter(this, Resource.Layout.ListViewTemplate1, data);
 
-
-			/*		SetContentView(Resource.Layout.AssignmentsLayout); 
-			 * 
-			 * 
-			 * 
-				var listView = FindViewById<ListView>(Resource.Id.assignmentsListView);
-				var data = new string[] { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
-				listView.Adapter = new ArrayAdapter (this, Resource.Layout.AssignmentItemLayout, data);
-*/
-
-
-
-			/*		SetContentView(Resource.Layout.MainLayoutTest); 
-
-			assignmentsListView = FindViewById<ListView> (Resource.Id.mainLayoutTestListView);
-			var data = new string[] { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
-			assignmentsListView.Adapter = new ArrayAdapter (this, Resource.Layout.ListViewTemplate1, data);
-			*/
-			int count = 0;
-			//	Button button = FindViewById<Button> (Resource.Id.button1);
-			//	button.Text = string.Format ("{0} clicks!!", count);
-
-
-
-
-
-
-			//assignmentActiveLayout = FindViewById<LinearLayout> (Resource.Id.assignmentSelectedItem);
-			//var view = new View (this);
-			//view = inflator.Inflate (Resource.Layout.AssignmentItemLayout, null);
-			//assignmentActiveLayout.AddView (view);
-
-
 			SetContentView(Resource.Layout.MainLayoutTest); 
 			assignmentsListView = FindViewById<ListView> (Resource.Id.mainLayoutTestListView);
-			//assignmentActiveLayout = FindViewById<LinearLayout> (Resource.Id.assignmentSelectedItem);
 
+	        assignmentActiveLayout = FindViewById<LinearLayout> (Resource.Id.assignmentSelectedItem);
 
+		//	var data = new string[] { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+ 
+         
+     
 
-			//	var view = new View (this);
-			//	LayoutInflater inflator = (LayoutInflater)GetSystemService (Context.LayoutInflaterService);
-			//		view = inflator.Inflate (Resource.Layout.AssignmentItemLayout, null);
-			//	assignmentActiveLayout.AddView (view);
-			//	view.LayoutParameters = new LinearLayout.LayoutParams (LinearLayout.LayoutParams.FillParent, LinearLayout.LayoutParams.WrapContent);
-			//view.SetBackgroundDrawable (Resources.GetDrawable (Resource.Drawable.active_assignment_selector));
+            var view = new View (this);
+            LayoutInflater inflator = (LayoutInflater)GetSystemService (Context.LayoutInflaterService);
+            view = inflator.Inflate (Resource.Layout.AssignmentItemLayout, null);
+            assignmentActiveLayout.AddView (view);
+            view.LayoutParameters = new LinearLayout.LayoutParams (LinearLayout.LayoutParams.FillParent, LinearLayout.LayoutParams.WrapContent);
+            view.SetBackgroundDrawable (Resources.GetDrawable (Resource.Drawable.active_assignment_selector));
 
+            number = view.FindViewById<TextView>(Resource.Id.assignmentItemNumber);
+            job = view.FindViewById<TextView>(Resource.Id.assignmentJob);
+            name = view.FindViewById<TextView>(Resource.Id.assignmentName);
+            phone = view.FindViewById<TextView>(Resource.Id.assignmentPhone);
+            address = view.FindViewById<TextView>(Resource.Id.assignmentAddress);
+           //   buttonLayout = view.FindViewById<LinearLayout>(Resource.Id.assignmentButtonLayout);
+          //  timerLayout = view.FindViewById<LinearLayout>(Resource.Id.assignmentTimerLayout);
+          //  activeSpinner = view.FindViewById<Spinner>(Resource.Id.assignmentStatus);
+          //  spinnerImage = view.FindViewById<ImageView>(Resource.Id.assignmentStatusImage);
+         //   timer = view.FindViewById<ToggleButton>(Resource.Id.assignmentTimer);
+            timerText = view.FindViewById<TextView>(Resource.Id.assignmentTimerText);
 
-			var data = new string[] { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
-			assignmentsListView.Adapter = new ArrayAdapter (this, Resource.Layout.AssignmentItemLayout,Resource.Id.assignmentItemNumber, data);
-			//assignmentsListView.Adapter = new ArrayAdapter (this, Resource.Layout.AssignmentItemLayout, data);
+            for (int i = 1; i < 110; i++)
+            {
+                list_item_number.Add(i);
+                list_assingment_name.Add("assingment_name");
+                list_assingment_job.Add("aasignmentJob");
+                list_assingment_phone.Add("assingment_phone");
+                list_assingment_Address.Add("assingment_Address");
+            }
+           
 
+            number.Text ="1";
+            job.Text = string.Format("#{0} {1}\n{2}", "JobNumber", "StartDate","CompanyName");
+            name.Text = "ContactName";
+            phone.Text = "ContactPhone";
+            address.Text = string.Format("{0}\n{1}, {2} {3}", "assignment.Address", "assignment.City", "assignment.State", "assignment.Zip");
+          //  assignmentActiveLayout.Visibility = ViewStates.Visible;
 
-
-
-
+    //  assignmentsListView.Adapter = new ArrayAdapter (this, Resource.Layout.AssignmentItemLayout, data);   //   this produces cast error
+               assignmentsListView.Adapter = new ArrayAdapter(this, Resource.Layout.AssignmentItemLayout, Resource.Id.assignmentItemNumber, list_item_number);
+             //   assignmentNameListView.Adapter = new ArrayAdapter(this, Resource.Layout.AssignmentItemLayout, Resource.Id.assignmentName, list_assingment_name);
+              //  assignmentJobListView.Adapter = new ArrayAdapter(this, Resource.Layout.AssignmentItemLayout, Resource.Id.assignmentJob, list_assingment_job);
+              //  assignmentPhoneListView.Adapter = new ArrayAdapter(this, Resource.Layout.AssignmentItemLayout, Resource.Id.assignmentPhone, list_assingment_phone);
+             //   assignmentAddressListView.Adapter = new ArrayAdapter(this, Resource.Layout.AssignmentItemLayout, Resource.Id.assignmentAddress, list_assingment_Address);
+               
 
         }
     }
